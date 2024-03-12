825557898951b1fdcf7fc0166a54072af0bf2870
Dhruvin151104 <131797669+Dhruvin151104@users.noreply.github.com>
2024-03-12 22:28:11 +0530

    Update App.jsx

--- Ethos-Bank/src/App.jsx
+++ Ethos-Bank/src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Login from './components/Login'
+import 
 
 function App() {
   return (
     <>
-      <Login />
+      
     </>
   )
 }
