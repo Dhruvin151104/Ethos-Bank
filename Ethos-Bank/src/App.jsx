60a377f96ff9eccb82ad09259fa10c462ddc594e
Dhruvin151104 <131797669+Dhruvin151104@users.noreply.github.com>
2024-03-13 00:59:09 +0530

    Added some photos

--- Ethos-Bank/src/App.jsx
+++ Ethos-Bank/src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import 
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <>
-      
+      <Navbar />
     </>
   )
 }
---
+++ Ethos-Bank/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Logo from "../assets/logo.svg";
+
+function Navbar() {
+    const color = "#0B1E2F";
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Navbar;
