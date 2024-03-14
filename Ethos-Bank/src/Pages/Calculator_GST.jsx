529c056afe3e65033aea7c54341f2596b220461b
Jaydatt <jaydattsonijs@gmail.com>
2024-03-14 10:38:15 +0530

    Calculator Started

--- Ethos-Bank/src/Pages/Calculator_GST.jsx
+++ Ethos-Bank/src/Pages/Calculator_GST.jsx
@@ -1,21 +1,42 @@
-import React from 'react'
+import React from "react";
+
+function button(val) {
+  return (
+    <>
+      <div className=" w-12 h-12 bg-slate-600 cursor-pointer flex justify-center items-center px-8 py-3 rounded-md font-[Poppins]">
+        <button type="button" onClick={(e)=>{
+          
+        }}>{val}%</button>
+      </div>
+    </>
+  );
+}
 
 function Calculator_GST() {
   return (
     <>
-    <div className="justify-center bg-slate-700">
-        <div className="font-monospace text-semibold text-lg">GST Calculator: Online GST Calculator</div>
-        <p className="">Calculate GST anytime</p>
-        <form id='gst-calc-form'>
+      <div className="flex justify-center items-center bg-slate-300 w-full h-[60vh] gap-4">
+        <div className="font-monospace text-semibold text-lg">
+          GST Calculator: Online GST Calculator
+          <p className="">Calculate GST anytime</p>
+          <form id="gst-calc-form">
           <div className="form-column">
-            <div className="gst-panel">
-              Select Rates
+            <div className="gst-rates-panel">
+              <h3>Select Rates</h3>
+            </div>
+            <div className="gst-rates-block flex gap-3">
+              {button(3)}
+              {button(5)}
+              {button(12)}
+              {button(18)}
+              {button(28)}
             </div>
           </div>
         </form>
-    </div>   
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Calculator_GST
+export default Calculator_GST;
