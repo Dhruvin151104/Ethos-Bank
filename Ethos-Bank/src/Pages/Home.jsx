b645bdc4551a4ba7a1783bc2fa99c73f2e8a6554
Dhruvin151104 <131797669+Dhruvin151104@users.noreply.github.com>
2024-03-21 11:44:09 +0530

    Home Page styling

--- Ethos-Bank/src/Pages/Home.jsx
+++ Ethos-Bank/src/Pages/Home.jsx
@@ -1,24 +1,35 @@
 import React from "react";
 import LoginButton from "../components/LoginButton";
 import test1 from "../assets/test1.jpg";
-import test2 from "../assets/test3.png"
-import test_svg from "../assets/test_svg.svg"
+import test2 from "../assets/test3.png";
+import test_svg from "../assets/test_svg.svg";
 
 function Home() {
-  const card = (props)=>{
-    return(
-      <div style={{width:props.width,height:props.height}} className="rounded-2xl shadow-xl bg-white flex flex-col justify-center items-center gap-4 hover:bg-slate-100 duration-200 ease-linear border-b-sky-500 border-b-4">
-        <div className="w-full mt-4" style={{height:props.imgHeight}}>
-          <img src={props.src} alt="" className="h-full m-auto"/>
+  const element = (props) => {
+    return (
+      <div
+        className="rounded-xl flex justify-evenly items-center shadow-md bg-gray-50 hover:bg-sky-50 duration-100 ease-linear hover:outline outline-2 outline-sky-600"
+        style={{ width: props.width, height: props.height }}
+      >
+        <div className="h-full w-[30%]">
+          <img src={props.img} className="h-full" />
         </div>
-        <div className=" h-2/5 flex items-center justify-center flex-col gap-3">
-          <p className="text-3xl font-medium">Heading</p>
-          <p className="text-md px-4 overflow-hidden text-center font-normal">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus, voluptates.</p>
+        <div className="h-full w-[70%] pl-6 pr-2 flex flex-col justify-center items-start gap-3">
+          <p className="text-3xl font-semibold">{props.heading}</p>
+          <p className="text-md text-gray-500">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt,
+            labore.
+          </p>
+          <a
+            href=""
+            className="text-md hover:text-sky-500 duration-200 ease-in-out font-semibold cursor-pointer"
+          >
+            Know More &rarr;
+          </a>
         </div>
-        {props.link && <a href="" className="text-lg hover:text-sky-500 duration-200 ease-in-out font-semibold">Know More &rarr;</a>}
       </div>
-    )
-  }
+    );
+  };
 
   return (
     <div className="font-[Poppins] bg-main-theme">
@@ -46,13 +57,56 @@ function Home() {
         ></div>
       </div>
 
-      <div className="h-[70vh]  flex justify-center items-center mt-32">
-        <div className="h-[90%] w-full  flex flex-col justify-center items-center">
-          <p className="text-4xl font-semibold text-[#154166]">Services</p>
-          <div className="w-full h-full flex justify-around items-center flex-wrap">
-          {card({height:"93%",width:"26%",src:test_svg,link:true,imgHeight:"25%",heading:"",description:""})}
-          {card({height:"93%",width:"26%",src:test_svg,link:true,imgHeight:"25%",heading:"",description:""})}
-          {card({height:"93%",width:"26%",src:test_svg,link:true,imgHeight:"25%",heading:"",description:""})}
+      <div className=" mt-16">
+        <div className=" w-[100%] text-5xl font-normal flex flex-col justify-center gap-1 pl-20">
+            <p>The all in one <span className=" text-blue-500 font-medium"> finance platform </span></p>
+            <p>you've been looking for...</p>
+        </div>
+        <div className="h-[100vh]  flex justify-center items-center mt-10 bg-white mx-20">
+          <div className="h-[90%] w-full  flex flex-col justify-center items-center">
+            <p className="text-4xl font-semibold text-[#154166] mt-10">
+              Services
+            </p>
+            <div className="w-[90%] h-full grid grid-cols-2 grid-rows-2">
+              <div className="h-full w-full flex items-center justify-center">
+                {element({
+                  width: "90%",
+                  height: "80%",
+                  img: test_svg,
+                  heading: "Loans",
+                })}
+              </div>
+              <div className="h-full w-full flex items-center justify-center">
+                {element({
+                  width: "90%",
+                  height: "80%",
+                  img: test_svg,
+                  heading: "Calculators",
+                })}
+              </div>
+              <div className="h-full w-full col-start-2 row-span-2 row-start-1 flex items-center justify-center">
+                <div className="rounded-xl flex justify-evenly items-center shadow-md bg-gray-50 hover:bg-sky-50 duration-100 ease-linear hover:outline outline-2 outline-sky-600 h-[90%] w-4/5 flex-col">
+                  <div className="h-[30%] w-full">
+                    <img src={test_svg} className="h-full m-auto" />
+                  </div>
+                  <div className="h-[50%] w-full pl-6 pr-2 flex flex-col justify-start pt-14 items-center gap-8">
+                    <p className="text-3xl font-semibold">Payments</p>
+                    <p className="text-lg text-gray-500">
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                      Reprehenderit reiciendis minima fugit nulla laborum
+                      corporis odit laudantium in voluptate et Lorem ipsum dolor
+                      sit, amet consectetur adipisicing elit. Iure, cum!
+                    </p>
+                  </div>
+                  <a
+                    href=""
+                    className="text-xl hover:text-sky-500 duration-200 ease-in-out font-semibold cursor-pointer"
+                  >
+                    Know More &rarr;
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
