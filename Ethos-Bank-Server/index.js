58e6223c24fb8fd7ffdfaf50a6565a53c910ec93
Jaydatt <jaydattsonijs@gmail.com>
2024-03-27 12:18:50 +0530

    Structured Server Co-authored-by: Dhruvin151104 <Dhruvin151104@users.noreply.github.com>

--- Ethos-Bank-Server/index.js
+++ Ethos-Bank-Server/index.js
@@ -1,29 +1,20 @@
 import express from "express"
 import cors from "cors"
 import mongoose from "mongoose"
-import userModel from "./models/users.js"
-import Mailer from "./Mailer/mailer.js"
+import Router from "./Routes/userRoutes.js"
 
 const app = express()
 const PORT = 5174
 
 app.use(express.json());
 app.use(cors());
+app.use('/', Router);
 
-mongoose.connect("mongodb://0.0.0.0:27017/demo-email")
-
-app.post('/login', (req, res) => {
-    const {email} = (req.body);
-    userModel.findOne({email:email})
-    .then(user => {
-        if(user){
-            // console.log(email);
-            Mailer(email);
-            res.json("ok")
-        }else{
-            res.json("No User found in DB")
-        }
-    })
-})
+try {
+    const database = await mongoose.connect("mongodb://0.0.0.0:27017/demo-email")
+    console.log("Database Connected");
+} catch (error) {
+    console.log("Database not connected",error);
+}
 
 app.listen(PORT, () => {console.log('Server is Running');})
