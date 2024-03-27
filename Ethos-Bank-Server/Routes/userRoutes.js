dd31b931925da937841bdd72b82dce689db2488d
Jaydatt <jaydattsonijs@gmail.com>
2024-03-27 17:07:36 +0530

    otp verification

--- Ethos-Bank-Server/Routes/userRoutes.js
+++ Ethos-Bank-Server/Routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { loginController,otpController } from "../Controllers/userController.js";
+import { loginController,otpController, confirmController } from "../Controllers/userController.js";
 
 const Router = express.Router();
 
 Router.post("/login", loginController);
 Router.post("/login/otp", otpController);
+Router.post("/login/details", confirmController);
 
 export default Router;
