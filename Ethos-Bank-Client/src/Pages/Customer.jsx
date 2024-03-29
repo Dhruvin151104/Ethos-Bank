61163a4242339ff16f20614530b66fd9aebacee6
Jaydatt <jaydattsonijs@gmail.com>
2024-03-29 11:17:18 +0530

    Profile Page frontend done

--- Ethos-Bank-Client/src/Pages/Customer.jsx
+++ Ethos-Bank-Client/src/Pages/Customer.jsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import rupee from "../assets/rupee-svg.svg";
 import debitCard from "../assets/debitCard.png";
+import viewDetails from "../assets/viewDetaiils.svg"
+import changePIN from "../assets/profilePIN.svg"
+import payments from "../assets/profilepayments.svg"
 
 function Customer() {
   const [showBalance, setshowBalance] = useState(false);
@@ -9,9 +12,9 @@ function Customer() {
 
   const buttons = (props) => {
     return (
-      <div className=" flex flex-col h-full justify-center items-center gap-9 w-[30%]">
-        <button className="text-white text-lg h-[7rem] w-[7rem] rounded-full bg-green-600 hover:bg-green-500 duration-300 ease-linear font-semibold">
-          img
+      <div className=" flex flex-col h-full justify-center items-center gap-6 w-[30%]">
+        <button className="text-white text-lg rounded-full bg-sky-600 hover:bg-sky-400 duration-300 ease-linear font-semibold w-[6.5rem] h-[6.5rem] flex items-center justify-center">
+          <img src={props.img} alt="" className=" h-1/2" />
         </button>
         <p>{props.txt}</p>
       </div>
@@ -20,26 +23,28 @@ function Customer() {
 
   const record = (props) => {
     return (
-      <div className={`w-full ${props.height} bg-slate-400 gap-1 flex p-1`}>
-          {description({name:props.first, width:"34%", bgcolor:"bg-slate-200"})}
-          {description({name:props.second, width:"17%", bgcolor:"bg-slate-200"})}
-          {description({name:props.third, width:"17%", bgcolor:"bg-slate-200"})}
-          {description({name:props.fourth, width:"17%", bgcolor:"bg-slate-200"})}
-          {description({name:props.fifth, width:"17%", bgcolor:"bg-slate-200"})}
+      <div className={`w-full  bg-white gap-1 flex p-1 ${props.border == 1 ? "border-b-[1px] border-sky-300" : ""}`}
+      style={{height:props.height}}>
+          {description({name:props.first, width:"34%", bgcolor:props.bgcolor})}
+          {description({name:props.second, width:"17%", bgcolor:props.bgcolor})}
+          {description({name:props.third, width:"17%", bgcolor:props.bgcolor})}
+          {description({name:props.fourth, width:"17%", bgcolor:props.bgcolor})}
+          {description({name:props.fifth, width:"17%", bgcolor:props.bgcolor})}
         </div>
     )
   }
 
   const description = (props) => {
     return (
-      <div className={`h-full ${props.bgcolor} flex justify-start font-medium items-center rounded-sm pl-2`} style={{width:props.width}}>{props.name}</div>
+      <div className={`${props.bgcolor} flex justify-start font-medium items-center rounded-sm pl-2`} style={{width:props.width}}>{props.name}</div>
     )
   }
 
   return (
-    <div className="h-max px-20 w-full bg-main-theme flex flex-col justify-center gap-10 items-center py-10 font-[Poppins] ">
-      <div className="h-[50vh] w-full bg-white shadow-xl flex justify-evenly items-center rounded-2xl">
-        <div className="h-[80%] w-1/2 bg-slate-100 rou flex pl-10 font-semibold text-lg rounded-2xl">
+    <div className="h-max px-20 w-full bg-main-theme flex flex-col justify-center gap-20 items-center py-10 font-[Poppins] ">
+      {/* Profile */}
+      <div className="h-[50vh] w-full bg-white shadow-md flex justify-evenly items-center rounded-2xl">
+        <div className="h-[80%] w-1/2 bg-slate-100 flex pl-10 font-semibold text-lg rounded-2xl">
           <div className="w-1/5 flex flex-col justify-evenly">
             <p>Acc. No. </p>
             <p>Name</p>
@@ -95,33 +100,44 @@ function Customer() {
           </div>
         </div>
       </div>
-      <div className="h-[50vh] w-full  shadow-xl flex justify-evenly items-center rounded-2xl">
-        <div className="h-[80%] w-[93.5%] bg-white rou flex font-semibold text-lg rounded-2xl gap-10">
-          <div className="h-full w-[40%] flex bg-green-100 rounded-2xl">
+      {/* Card */}
+      <div className="h-[50vh] w-full bg-white shadow-md flex justify-evenly items-center rounded-2xl">
+        <div className="h-[80%] w-[93.5%]  flex font-semibold text-lg rounded-2xl gap-10">
+          <div className="h-full w-[40%] flex rounded-2xl">
             <img
               src={debitCard}
               alt=""
               className="w-full h-full object-cover"
             />
           </div>
-          <div className="h-full w-[60%] flex justify-evenly items-center gap-6 bg-red-100 rounded-2xl">
-            {buttons({txt:"View Details"})}
-            {buttons({txt:"Change PIN"})}
-            {buttons({txt:"Payments"})}
+          <div className="h-full w-[60%] flex justify-evenly items-center gap-6 bg-slate-100 rounded-2xl">
+            {buttons({txt:"View Details", img:viewDetails})}
+            {buttons({txt:"Change PIN", img:changePIN})}
+            {buttons({txt:"Payments", img:payments})}
           </div>
         </div>
       </div>
-      <div className="h-[80vh] w-full bg-orange-300 overflow-y-auto">
-        <div className="h-[15%] w-full bg-blue-200 flex justify-center items-center">
-          <p className=" font-semibold text-2xl">Transaction Details</p>
+      {/* Transaction */}
+      <div className="h-[70vh] w-full bg-white shadow-md rounded-2xl mb-4">
+        <div className=" h-[15%] w-full rounded-2xl flex justify-center items-center">
+          <p className=" font-medium text-3xl">Transaction Details</p>
+        </div>  
+        <div className="w-full bg-red-300 h-[10%] flex justify-center items-center">
+          {record({height:"100%", first:"Payment Details", second:"Status", third:"Amount", fourth:"Order No.", fifth:"Order Total", bgcolor:"bg-slate-300"})}
         </div>
-        <div className="flex flex-col gap-2">
-        <div className="w-full h-full bg-slate-400 gap-1 flex flex-col p-1 ">
-          {record({height:"10%", first:"Payment Details", second:"Status", third:"Amount", fourth:"Order No.", fifth:"Order Total"})}
-        </div>
-        <div className="bg-red-200 h-full w-full flex flex-col justify-start items-center gap-1">
-          {record({height:"20%", first:"Payment Details", second:"Status", third:"Amount", fourth:"Order No.", fifth:"Order Total"})}
-        </div>
+        <div className="w-full bg-green-200 h-[75%] rounded-2xl overflow-y-auto ">
+          {record({height:"15%", first:"Received 1000 Rs", second:"Successfull", third:"1000", fourth:"1", fifth:"1000", border:1})}
+          {record({height:"15%", first:"Received 1000 Rs", second:"Successfull", third:"1000", fourth:"1", fifth:"1000", border:1})}
+          {record({height:"15%", first:"Received 1000 Rs", second:"Successfull", third:"1000", fourth:"1", fifth:"1000", border:1})}
+          {record({height:"15%", first:"Received 1000 Rs", second:"Successfull", third:"1000", fourth:"1", fifth:"1000", border:1})}
+          {record({height:"15%", first:"Received 1000 Rs", second:"Successfull", third:"1000", fourth:"1", fifth:"1000", border:1})}
+          {record({height:"15%", first:"Received 1000 Rs", second:"Successfull", third:"1000", fourth:"1", fifth:"1000", border:1})}
+          {record({height:"15%", first:"Received 1000 Rs", second:"Successfull", third:"1000", fourth:"1", fifth:"1000", border:1})}
+          {record({height:"15%", first:"Received 1000 Rs", second:"Successfull", third:"1000", fourth:"1", fifth:"1000", border:1})}
+          {record({height:"15%", first:"Received 1000 Rs", second:"Successfull", third:"1000", fourth:"1", fifth:"1000", border:1})}
+          {record({height:"15%", first:"Received 1000 Rs", second:"Successfull", third:"1000", fourth:"1", fifth:"1000", border:1})}
+          {record({height:"15%", first:"Received 1000 Rs", second:"Successfull", third:"1000", fourth:"1", fifth:"1000", border:1})}
+          {record({height:"15%", first:"Received 1000 Rs", second:"Successfull", third:"1000", fourth:"1", fifth:"1000", border:1})}
         </div>
       </div>
     </div>
