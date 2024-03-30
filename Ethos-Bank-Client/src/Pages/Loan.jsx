b5d1295d543600b8f2dd769467b57ffe24afd970
Dhruvin151104 <131797669+Dhruvin151104@users.noreply.github.com>
2024-03-31 00:44:01 +0530

    Loan Page styling

--- Ethos-Bank-Client/src/Pages/Loan.jsx
+++ Ethos-Bank-Client/src/Pages/Loan.jsx
@@ -7,9 +7,26 @@ import loanCalc from "../assets/loanCalc.png";
 import Circular_Progress from "../components/Circular_Progress";
 
 function Loan() {
-  const [loanAmount, setloanAmount] = useState();
-  const [tenure, settenure] = useState();
-  const [rateOfInterest, setrateOfInterest] = useState();
+  const [loanAmount, setloanAmount] = useState("");
+  const [tenure, settenure] = useState("");
+  const [rateOfInterest, setrateOfInterest] = useState("");
+
+  // EMI calculation
+  const EMI = () => {
+    if (!loanAmount || !tenure || !rateOfInterest)
+      return { emi: 0, interest: 0, percent: 0 };
+    const temp = (1 + rateOfInterest / 1200) ** (tenure * 12);
+    const montlyEmi =
+      loanAmount * (rateOfInterest / 1200) * (temp / (temp - 1));
+    const total = montlyEmi * tenure * 12;
+    const interest = total - loanAmount;
+    const percent = (interest / total) * 252;
+    return {
+      emi: Math.round(montlyEmi),
+      interest: Math.round(interest),
+      percent: percent.toFixed(2),
+    };
+  };
 
   return (
     <div className="w-full min-h-[100vh] px-20 py-10 flex flex-col justify-center items-center gap-20 font-[Poppins]">
@@ -34,45 +51,89 @@ function Loan() {
       </div>
 
       {/* Calculator Part */}
-      <div className="w-full h-[70vh] bg-white shadow-inner rounded-2xl overflow-hidden flex">
-        <div className="h-full w-[25%] flex justify-center items-center">
+      <div className="w-full h-[80vh] bg-white shadow-inner rounded-2xl overflow-hidden flex py-10 justify-evenly px-2">
+        {/* Image div */}
+        <div className="h-full w-[20%] flex justify-center items-center">
           <img src={loanCalc} alt="" className="h-2/5" />
         </div>
-        <div className="h-full w-[45%]  p-4">
-          <div className="h-full w-full rounded-2xl shadow-inner flex flex-col justify-center items-start pl-4 bg-slate-100 gap-1">
-            <div className=" h-[10%] font-semibold text-3xl text-[#154166] flex justify-center items-center w-full">Monthly EMI Calculator</div>
-            <div className="h-[90%] flex flex-col justify-center items-start pl-4 gap-10 w-full">
-          <Input
-              type="number"
-              settext={setloanAmount}
-              text={loanAmount}
-              placeholder="₹ 10,000"
-              heading="Loan Amout"
-            />
-          <Input
-              type="number"
-              settext={settenure}
-              text={tenure}
-              placeholder="1 years"
-              heading="Tenure (Years)"
-            />
-          <Input
-              type="number"
-              settext={setrateOfInterest}
-              text={rateOfInterest}
-              placeholder="0.5%"
-              heading="Interest Rate (% P.A.)"
-            />
+
+        {/* Input+Output */}
+        <div className="h-full w-[75%] bg-slate-100 shadow-inner rounded-2xl flex">
+          {/* Input div */}
+          <div className="h-full w-[60%] p-4">
+            <div className="h-full w-full flex flex-col justify-center items-start pl-4 gap-1">
+              <div className=" h-[20%] font-semibold text-3xl text-[#154166] flex justify-center items-center w-full">
+                Monthly EMI Calculator
+              </div>
+              <div className="h-[80%] flex flex-col justify-center items-start pl-4 gap-7 w-full">
+                <Input
+                  type="number"
+                  settext={setloanAmount}
+                  text={loanAmount}
+                  placeholder="₹ 10,000"
+                  heading="Loan Amout"
+                />
+                <Input
+                  type="number"
+                  settext={settenure}
+                  text={tenure}
+                  placeholder="1 years"
+                  heading="Tenure (Years)"
+                />
+                <Input
+                  type="number"
+                  settext={setrateOfInterest}
+                  text={rateOfInterest}
+                  placeholder="0.5%"
+                  heading="Interest Rate (% P.A.)"
+                />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="h-full w-[30%] bg-red-600">
-            <Circular_Progress
-                height="30vh"
-                width="11vw"
-                value1="0"
-                value2="200"
-              />
+
+          {/* Result div */}
+          <div className="h-full w-[45%]  p-4">
+            <div className="h-full w-full flex flex-col justify-center items-center">
+              <div className="h-[90%] w-[95%] bg-white shadow-inner rounded-2xl overflow-hidden flex flex-col items-center">
+                <div className="h-[16%] w-full bg-sky-600 flex justify-center items-center text-3xl font-semibold  text-white">
+                  Monthly EMI
+                </div>
+                <div className="flex justify-start items-center flex-col gap-6 pt-8 w-full">
+                  <div className="text-4xl text-sky-600 font-semibold w-full text-center">
+                    ₹ {EMI().emi}
+                  </div>
+
+                  <div className="text-md font[Poppins] text-black font-semibold w-[90%] h-[26vh] rounded-2xl flex items-center justify-center shadow-inner bg-gray-100">
+                    <div className="w-full h-[25vh] flex justify-around items-center">
+                      <Circular_Progress
+                        height="20vh"
+                        width="9vw"
+                        value1="0"
+                        value2={EMI().percent}
+                      />
+                      <div className="flex flex-col gap-6">
+                        <div className="flex items-start flex-col">
+                          <div className="w-full flex gap-2 items-center">
+                            <div className=" w-4 h-4 bg-sky-200"></div>
+                            <p>Principal Amount</p>
+                          </div>
+                          <p className=" pl-7">₹ {loanAmount ? loanAmount : 0}</p>
+                        </div>
+                        <div className="flex items-start flex-col">
+                          <div className="w-full flex gap-2 items-center">
+                            <div className=" w-4 h-4 bg-sky-600 "></div>
+                            <p>Total Interest</p>
+                          </div>
+                          <p className=" pl-7">₹ {EMI().interest}</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="h-[10%] w-[90%] flex flex-col mt-4"><LoginButton name="Apply Now" y="0.7rem" /></div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
