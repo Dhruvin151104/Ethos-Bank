140c2bf37f76fca19e86d8f7aae132a2e8ec6780
Dhruvin151104 <131797669+Dhruvin151104@users.noreply.github.com>
2024-03-31 12:35:13 +0530

    Loan Page styling almost done

--- Ethos-Bank-Client/src/components/Input.jsx
+++ Ethos-Bank-Client/src/components/Input.jsx
@@ -11,7 +11,7 @@ function Input(props) {
         onChange={(e) => {
           props.settext(e.target.value);
         }}
-        className="text-[#154166] shadow-md font-semibold h-[7vh] w-11/12 text-2xl pl-5 font-[Poppins] rounded-md remove-arrow focus:outline-sky-600 outline-8"
+        className="text-[#154166] shadow-md font-semibold h-[7vh] w-[80%] text-xl pl-5 font-[Poppins] rounded-md remove-arrow focus:outline-sky-600 outline-8 placeholder:font-medium placeholder:text-xl"
         placeholder={props.placeholder}
         value={props.text}
       />
