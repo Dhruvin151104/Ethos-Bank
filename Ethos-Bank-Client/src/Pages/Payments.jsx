55029a3c5f74e2b48f3dba8ac18ada7e87c9cda4
Jaydatt <jaydattsonijs@gmail.com>
2024-04-12 13:37:19 +0530

    minor changes

--- Ethos-Bank-Client/src/Pages/Payments.jsx
+++ Ethos-Bank-Client/src/Pages/Payments.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 function Payments() {
   return (
-    <div className='h-[100vh] w-full bg-green-200'>
-        
+    <div className='h-[100vh] w-full bg-green-200 font-[Poppins]'>
+      <div className='flex flex-col h-[50%] justify-center items-center'>
+      <div className='h-[15%] w-[90%] bg-white shadow-md flex rounded-lg'>
+            <div className='w-1/2 h-full flex justify-center items-center border-r-2 border-r-sky-600'>
+              <p className='text-2xl'>Money Transfer</p>
+            </div>
+            <div className='w-1/2 h-full flex justify-center items-center'>
+              <p className='text-2xl'>NEFT</p>
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
